3a89e9e16a8c6a9053143b0e440b50098e1bc507
nyarla <nyarla@thotep.net>
2013-02-21 16:35:37 +0900

    more bugfix

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 class bitcasa {
   package { 'Bitcasa':
     provider => 'apple',
-    source   => 'https://www.bitcasa.com/download/mac'
+    source   => 'http://dist.bitcasa.com/Bitcasa_1037.pkg'
   }
 }
