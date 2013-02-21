547463338d65c5e58fe98e344696b4fe8a53099c
nyarla <nyarla@thotep.net>
2013-02-21 18:08:25 +0900

    updated

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,10 @@
 class bitcasa {
-  require boxen::config
-
-  exec { "curl -kL https://www.bitcasa.com/download/mac -o '${boxen::config::cachedir}/Bitcasa.pkg'":
+  exec { "curl -kL https://www.bitcasa.com/download/mac -o '/Library/Receipts/Bitcasa.pkg'":
     path => [ '/usr/bin' ]
   }
 
   package { 'Bitcasa':
     provider => 'apple',
-    source   => "${boxen::config::cachedir}/Bitcasa.pkg",
-    ensure   => 'installed',
+    source   => '/Library/Receipts/Bitcasa.pkg'
   }
 }
