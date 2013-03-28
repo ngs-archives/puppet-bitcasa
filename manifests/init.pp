a00505d012f1b878d0e21ddb851911e9e1126ee6
nyarla <nyarla@thotep.net>
2013-03-28 13:59:11 +0900

    update

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,15 @@
 class bitcasa {
-  exec { "curl -kL https://www.bitcasa.com/download/mac -o '/Library/Receipts/Bitcasa.pkg'":
-    path   => [ '/usr/bin', '/bin' ],
-    onlyif => "sh -c '[ ! -f /Library/Receipts/Bitcasa.pkg ]'",
+  $bitcasa_version = '1061';
+
+  exec { "bitcasa-fetch-installer":
+    command => "curl -kL https://www.bitcasa.com/download/mac -o '/Library/Receipts/Bitcasa_${bitcasa_version}.pkg'"
+    path    => [ '/usr/bin', '/bin' ],
+    onlyif  => "sh -c '[ ! -f /Lubrary/Receipts/Bitcasa_${bitcasa_version}.pkg ]'",
   }
 
   package { 'Bitcasa':
     provider => 'apple',
-    source   => '/Library/Receipts/Bitcasa.pkg'
+    source   => "/Library/Receipts/Bitcasa_${bitcasa_version}.pkg",
+    require  => Exec['bitcasa-fetch-installer'],
   }
 }
