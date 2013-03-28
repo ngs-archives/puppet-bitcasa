4118837a938f9560d7027c023031d78b615655cd
nyarla <nyarla@thotep.net>
2013-03-28 14:02:18 +0900

    fix mistake

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,8 @@
 class bitcasa {
-  $bitcasa_version = '1061';
+  $bitcasa_version = '1061'
 
   exec { "bitcasa-fetch-installer":
-    command => "curl -kL https://www.bitcasa.com/download/mac -o '/Library/Receipts/Bitcasa_${bitcasa_version}.pkg'"
+    command => "curl -kL https://www.bitcasa.com/download/mac -o '/Library/Receipts/Bitcasa_${bitcasa_version}.pkg'",
     path    => [ '/usr/bin', '/bin' ],
     onlyif  => "sh -c '[ ! -f /Lubrary/Receipts/Bitcasa_${bitcasa_version}.pkg ]'",
   }
