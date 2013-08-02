281e23f513ca3046ef3f574d16410aae1d004fa9
nyarla <nyarla@thotep.net>
2013-08-02 13:00:51 +0900

    init.pp: fix installer URI

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 class bitcasa {
   package { 'Bitcasa':
     provider => 'pkgdmg',
-    source   => 'https://www.bitcasa.com/download/mac',
+    source   => 'https://d1kbf262vwnlmm.cloudfront.net/Bitcasa_1.3.1207.dmg',
   }
 }
