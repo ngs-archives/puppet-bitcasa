035e5e3c601a30f5536209750866f002198293c1
Atsushi Nagase <a@ngs.io>
2014-05-04 13:17:26 +0900

    Update source URL

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class bitcasa {
   package { 'Bitcasa':
     provider => 'pkgdmg',
-    source   => 'https://d1kbf262vwnlmm.cloudfront.net/Bitcasa_1.4.1287.dmg',
+    source   => 'https://www.bitcasa.com/download/mac',
   }
 }
